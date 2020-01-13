7054d037fc54ac9c117dc4e272639b4a5a4c5a30
a.funicello3 <a.funicello3@studenti.unisa.it>
2020-01-13 18:15:31 +0100

    CheckStyle Fix

--- src/controller/ServletInoltra.java
+++ src/controller/ServletInoltra.java
@@ -18,53 +18,63 @@ import model.RequestCSDAO;
  */
 @WebServlet("/ServletInoltra")
 public class ServletInoltra extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public ServletInoltra() {
-        super();
-        // TODO Auto-generated constructor stub
+  private static final long serialVersionUID = 1L;
+
+  /**
+   * 
+   * 
+   * @see HttpServlet#HttpServlet()
+   */
+  public ServletInoltra() {
+    super();
+    // TODO Auto-generated constructor stub
+  }
+
+  /**
+   * Metodo DoGet.
+   * 
+   * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+   *      response)
+   */
+  protected void doGet(HttpServletRequest request, HttpServletResponse response)
+      throws ServletException, IOException {
+    doPost(request, response);
+  }
+
+  /**
+   * Meotodo DoPost. 
+   * 
+   * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+   *      response)
+   */
+  public void doPost(HttpServletRequest request, HttpServletResponse response)
+      throws ServletException, IOException {
+
+    Integer result = 0;
+    String error = "";
+    String content = "";
+
+    Integer id = Integer.parseInt(request.getParameter("id"));
+    RequestCSDAO rDAO = new RequestCSDAO();
+
+    try {
+      rDAO.doInoltraToAdmin(id);
+      content = "Richiesta inoltrata all'admin!";
+      result = 1;
+    } catch (Exception e) {
+      error = "Errore";
+      result = 0;
+      e.printStackTrace();
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		doPost(request, response);
-	}
-
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
-		Integer result = 0;
-		String error = "";
-		String content = "";
-		String redirect = "";
-		
-		Integer id = Integer.parseInt(request.getParameter("id"));
-		RequestCSDAO rDAO = new RequestCSDAO();
-		
-		try {
-			rDAO.doInoltraToAdmin(id);
-			content = "Richiesta inoltrata all'admin!";
-			result = 1;
-		} catch (Exception e) {
-			error = "Errore";
-			result = 0;
-			e.printStackTrace();
-		}
-		
-		JSONObject res = new JSONObject();
-		res.put("result", result);
-		res.put("error", error);
-		res.put("content", content);
-		res.put("redirect", redirect);
-		PrintWriter out = response.getWriter();
-		out.println(res);
-		response.setContentType("json");
-	}
+    String redirect = "";
+    JSONObject res = new JSONObject();
+    res.put("result", result);
+    res.put("error", error);
+    res.put("content", content);
+    res.put("redirect", redirect);
+    PrintWriter out = response.getWriter();
+    out.println(res);
+    response.setContentType("json");
+  }
 }
