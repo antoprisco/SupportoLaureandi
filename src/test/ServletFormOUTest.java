be7b0ac43960cf839a85ef8f8ed8c341be223048
ilcavaliere01 <pasqualeannarumma@gmail.com>
2020-01-13 17:08:45 +0100

    Checkstyle rispettato
    CheckStyle rispettato nei file di unit test: ServletCercaLMTest,  ServletChangeDataTest,  ServletFormCSTest, ServletFormLMTest, ServletFormOUTest, ServletViewReqCSATest, ServletViewReqCSTest

--- src/test/ServletFormOUTest.java
+++ src/test/ServletFormOUTest.java
@@ -1,15 +1,15 @@
 package test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import controller.ServletCercaLM;
 import controller.ServletFormLM;
 import controller.ServletFormOU;
 import interfacce.UserInterface;
-import model.Student;
-
 import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpSession;
+import model.Student;
 
 import org.json.simple.JSONObject;
 import org.junit.Before;
@@ -36,74 +36,268 @@ public class ServletFormOUTest extends Mockito {
   }
   
   @Test
-  public void test1() throws ServletException, IOException {
-	UserInterface utente = new Student("p.annarumma@studenti.unisa.it", "pasquale", "annarumma", 'M', "pippotto", 0);
-	session.setAttribute("user", utente);
-	request.addParameter("email", "p.annarumma@studenti.unisa.it");
-	request.addParameter("telefono", "3420966986");
+  public void testOk() throws ServletException, IOException {
+    UserInterface utente = 
+        new Student("a.napolii@studenti.unisa.it", "antonio", "napoli", 'M', "plutone", 0);
+    session.setAttribute("user", utente);
+    request.addParameter("nome", "pasquale");
+    request.addParameter("cognome", "annarumma");
+    request.addParameter("email", "a.napolii@studenti.unisa.it");
+    request.addParameter("telefono", "3420966986");
     request.addParameter("datanascita", "1994-10-10");
-    request.addParameter("skills", "[{\"skill\":\"java\",\"value\":\"basso\"},{\"skill\":\"c\",\"value\":\"medio\"},{\"skill\":\"mysql\",\"value\":\"alto\"}]");
-    request.addParameter("softSkills", "[{\"softskill\":\"\"},{\"softskill\":\"problem solving\"},{\"softskill\":\"ciaoooooooooooooo\"}]");
-    request.addParameter("lingue", "[{\"lang\":\"inglese\",\"value\":\"A1\"},{\"lang\":\"francese\",\"value\":\"A2\"},{\"lang\":\"tedesco\",\"value\":\"B1\"},{\"lang\":\"spagnolo\",\"value\":\"B2\"}]");
+    request.addParameter("skills", "[{\"skill\":\"java\",\"value\":\"basso\"},"
+        + "{\"skill\":\"c\",\"value\":\"medio\"},"
+        + "{\"skill\":\"mysql\",\"value\":\"alto\"}]");
+    request.addParameter("softSkills", "[{\"softskill\":\"\"},{\"softskill\":\"problem solving\"},"
+        + "{\"softskill\":\"ciaoooooooooooooo\"}]");
+    request.addParameter("lingue", "[{\"lang\":\"inglese\",\"value\":\"A1\"},"
+        + "{\"lang\":\"francese\",\"value\":\"A2\"},"
+        + "{\"lang\":\"tedesco\",\"value\":\"B1\"},{\"lang\":\"spagnolo\",\"value\":\"B2\"}]");
     servlet.doPost(request, response);
     assertEquals("json", response.getContentType());
   }
   
   @Test
-  public void test2() throws ServletException, IOException {
-	UserInterface utente = new Student("p.annarumma@studenti.unisa.it", "pasquale", "annarumma", 'M', "pippotto", 0);
-	//session.setAttribute("user", utente);
-	request.addParameter("email", "p.annarumma@studenti.unisa.it");
-	request.addParameter("telefono", "3420966986");
+  public void testReqExist() throws ServletException, IOException {
+    UserInterface utente = 
+        new Student("p.annarumma@studenti.unisa.it", "pasquale", "annarumma", 'M', "pippotto", 0);
+    session.setAttribute("user", utente);
+    request.addParameter("nome", "pasquale");
+    request.addParameter("cognome", "annarumma");
+    request.addParameter("email", "p.annarumma@studenti.unisa.it");
+    request.addParameter("telefono", "3420966986");
     request.addParameter("datanascita", "1994-10-10");
-    request.addParameter("skills", "[{\"skill\":\"java\",\"value\":\"basso\"},{\"skill\":\"c\",\"value\":\"medio\"},{\"skill\":\"mysql\",\"value\":\"alto\"}]");
-    request.addParameter("softSkills", "[{\"softskill\":\"\"},{\"softskill\":\"problem solving\"},{\"softskill\":\"ciaoooooooooooooo\"}]");
-    request.addParameter("lingue", "[{\"lang\":\"inglese\",\"value\":\"A1\"},{\"lang\":\"francese\",\"value\":\"A2\"},{\"lang\":\"tedesco\",\"value\":\"B1\"},{\"lang\":\"spagnolo\",\"value\":\"B2\"}]");
+    request.addParameter("skills", "[{\"skill\":\"java\",\"value\":\"basso\"},"
+        + "{\"skill\":\"c\",\"value\":\"medio\"},"
+        + "{\"skill\":\"mysql\",\"value\":\"alto\"}]");
+    request.addParameter("softSkills", "[{\"softskill\":\"\"},{\"softskill\":\"problem solving\"},"
+        + "{\"softskill\":\"ciaoooooooooooooo\"}]");
+    request.addParameter("lingue", "[{\"lang\":\"inglese\",\"value\":\"A1\"},"
+        + "{\"lang\":\"francese\",\"value\":\"A2\"},"
+        + "{\"lang\":\"tedesco\",\"value\":\"B1\"},{\"lang\":\"spagnolo\",\"value\":\"B2\"}]");
+    servlet.doPost(request, response);
+    assertEquals("json", response.getContentType());
+  }
+  
+  @Test
+  public void testNameLenght0() throws ServletException, IOException {
+    UserInterface utente = 
+        new Student("a.napoli@studenti.unisa.it", "antonio", "napoli", 'M', "plutone", 0);
+    session.setAttribute("user", utente);
+    request.addParameter("nome", "");
+    request.addParameter("cognome", "annarumma");
+    request.addParameter("email", "a.napoli@studenti.unisa.it");
+    request.addParameter("telefono", "3420966986");
+    request.addParameter("datanascita", "1994-10-10");
+    request.addParameter("skills", "[{\"skill\":\"java\",\"value\":\"basso\"},"
+        + "{\"skill\":\"c\",\"value\":\"medio\"},"
+        + "{\"skill\":\"mysql\",\"value\":\"alto\"}]");
+    request.addParameter("softSkills", "[{\"softskill\":\"\"},{\"softskill\":\"problem solving\"},"
+        + "{\"softskill\":\"ciaoooooooooooooo\"}]");
+    request.addParameter("lingue", "[{\"lang\":\"inglese\",\"value\":\"A1\"},"
+        + "{\"lang\":\"francese\",\"value\":\"A2\"},"
+        + "{\"lang\":\"tedesco\",\"value\":\"B1\"},{\"lang\":\"spagnolo\",\"value\":\"B2\"}]");
+    servlet.doPost(request, response);
+    assertEquals("json", response.getContentType());
+  }
+  
+  @Test
+  public void testNameLenghtOverflow() throws ServletException, IOException {
+    UserInterface utente = 
+        new Student("a.napoli@studenti.unisa.it", "antonio", "napoli", 'M', "plutone", 0);
+    session.setAttribute("user", utente);
+    request.addParameter("nome", "pasqualepasqualepasquale");
+    request.addParameter("cognome", "annarumma");
+    request.addParameter("email", "a.napoli@studenti.unisa.it");
+    request.addParameter("telefono", "3420966986");
+    request.addParameter("datanascita", "1994-10-10");
+    request.addParameter("skills", "[{\"skill\":\"java\",\"value\":\"basso\"},"
+        + "{\"skill\":\"c\",\"value\":\"medio\"},"
+        + "{\"skill\":\"mysql\",\"value\":\"alto\"}]");
+    request.addParameter("softSkills", "[{\"softskill\":\"\"},{\"softskill\":\"problem solving\"},"
+        + "{\"softskill\":\"ciaoooooooooooooo\"}]");
+    request.addParameter("lingue", "[{\"lang\":\"inglese\",\"value\":\"A1\"},"
+        + "{\"lang\":\"francese\",\"value\":\"A2\"},"
+        + "{\"lang\":\"tedesco\",\"value\":\"B1\"},{\"lang\":\"spagnolo\",\"value\":\"B2\"}]");
+    servlet.doPost(request, response);
+    assertEquals("json", response.getContentType());
+  }
+  
+  @Test
+  public void testNameFailFormat() throws ServletException, IOException {
+    UserInterface utente = 
+        new Student("a.napoli@studenti.unisa.it", "antonio", "napoli", 'M', "plutone", 0);
+    session.setAttribute("user", utente);
+    request.addParameter("nome", "p4squale");
+    request.addParameter("cognome", "annarumma");
+    request.addParameter("email", "a.napoli@studenti.unisa.it");
+    request.addParameter("telefono", "3420966986");
+    request.addParameter("datanascita", "1994-10-10");
+    request.addParameter("skills", "[{\"skill\":\"java\",\"value\":\"basso\"},"
+        + "{\"skill\":\"c\",\"value\":\"medio\"},"
+        + "{\"skill\":\"mysql\",\"value\":\"alto\"}]");
+    request.addParameter("softSkills", "[{\"softskill\":\"\"},{\"softskill\":\"problem solving\"},"
+        + "{\"softskill\":\"ciaoooooooooooooo\"}]");
+    request.addParameter("lingue", "[{\"lang\":\"inglese\",\"value\":\"A1\"},"
+        + "{\"lang\":\"francese\",\"value\":\"A2\"},"
+        + "{\"lang\":\"tedesco\",\"value\":\"B1\"},{\"lang\":\"spagnolo\",\"value\":\"B2\"}]");
+    servlet.doPost(request, response);
+    assertEquals("json", response.getContentType());
+  }
+  
+  @Test
+  public void testSurnameLenght0() throws ServletException, IOException {
+    UserInterface utente = 
+        new Student("a.napoli@studenti.unisa.it", "antonio", "napoli", 'M', "plutone", 0);
+    session.setAttribute("user", utente);
+    request.addParameter("nome", "pasquale");
+    request.addParameter("cognome", "");
+    request.addParameter("email", "a.napoli@studenti.unisa.it");
+    request.addParameter("telefono", "3420966986");
+    request.addParameter("datanascita", "1994-10-10");
+    request.addParameter("skills", "[{\"skill\":\"java\",\"value\":\"basso\"},"
+        + "{\"skill\":\"c\",\"value\":\"medio\"},"
+        + "{\"skill\":\"mysql\",\"value\":\"alto\"}]");
+    request.addParameter("softSkills", "[{\"softskill\":\"\"},{\"softskill\":\"problem solving\"},"
+        + "{\"softskill\":\"ciaoooooooooooooo\"}]");
+    request.addParameter("lingue", "[{\"lang\":\"inglese\",\"value\":\"A1\"},"
+        + "{\"lang\":\"francese\",\"value\":\"A2\"},"
+        + "{\"lang\":\"tedesco\",\"value\":\"B1\"},{\"lang\":\"spagnolo\",\"value\":\"B2\"}]");
+    servlet.doPost(request, response);
+    assertEquals("json", response.getContentType());
+  }
+  
+  @Test
+  public void testSurnameLeghtOverflow() throws ServletException, IOException {
+    UserInterface utente = 
+        new Student("a.napoli@studenti.unisa.it", "antonio", "napoli", 'M', "plutone", 0);
+    session.setAttribute("user", utente);
+    request.addParameter("nome", "pasquale");
+    request.addParameter("cognome", "annarummaannarummaannarumma");
+    request.addParameter("email", "a.napoli@studenti.unisa.it");
+    request.addParameter("telefono", "3420966986");
+    request.addParameter("datanascita", "1994-10-10");
+    request.addParameter("skills", "[{\"skill\":\"java\",\"value\":\"basso\"},"
+        + "{\"skill\":\"c\",\"value\":\"medio\"},"
+        + "{\"skill\":\"mysql\",\"value\":\"alto\"}]");
+    request.addParameter("softSkills", "[{\"softskill\":\"\"},{\"softskill\":\"problem solving\"},"
+        + "{\"softskill\":\"ciaoooooooooooooo\"}]");
+    request.addParameter("lingue", "[{\"lang\":\"inglese\",\"value\":\"A1\"},"
+        + "{\"lang\":\"francese\",\"value\":\"A2\"},"
+        + "{\"lang\":\"tedesco\",\"value\":\"B1\"},{\"lang\":\"spagnolo\",\"value\":\"B2\"}]");
+    servlet.doPost(request, response);
+    assertEquals("json", response.getContentType());
+  }
+  
+  @Test
+  public void testEmailLenght0() throws ServletException, IOException {
+    UserInterface utente = 
+        new Student("a.napolii@studenti.unisa.it", "antonio", "napoli", 'M', "plutone", 0);
+    session.setAttribute("user", utente);
+    request.addParameter("nome", "pasquale");
+    request.addParameter("cognome", "annarumma");
+    request.addParameter("email", "");
+    request.addParameter("telefono", "3420966986");
+    request.addParameter("datanascita", "1994-10-10");
+    request.addParameter("skills", "[{\"skill\":\"java\",\"value\":\"basso\"},"
+        + "{\"skill\":\"c\",\"value\":\"medio\"},"
+        + "{\"skill\":\"mysql\",\"value\":\"alto\"}]");
+    request.addParameter("softSkills", "[{\"softskill\":\"\"},{\"softskill\":\"problem solving\"},"
+        + "{\"softskill\":\"ciaoooooooooooooo\"}]");
+    request.addParameter("lingue", "[{\"lang\":\"inglese\",\"value\":\"A1\"},"
+        + "{\"lang\":\"francese\",\"value\":\"A2\"},"
+        + "{\"lang\":\"tedesco\",\"value\":\"B1\"},{\"lang\":\"spagnolo\",\"value\":\"B2\"}]");
+    servlet.doPost(request, response);
+    assertEquals("json", response.getContentType());
+  }
+  
+  @Test
+  public void testEmailFailFormat() throws ServletException, IOException {
+    UserInterface utente = 
+        new Student("a.napolii@studenti.unisa.it", "antonio", "napoli", 'M', "plutone", 0);
+    session.setAttribute("user", utente);
+    request.addParameter("nome", "pasquale");
+    request.addParameter("cognome", "annarumma");
+    request.addParameter("email", "a.napoli@studentii.unisa.it");
+    request.addParameter("telefono", "3420966986");
+    request.addParameter("datanascita", "1994-10-10");
+    request.addParameter("skills", "[{\"skill\":\"java\",\"value\":\"basso\"},"
+        + "{\"skill\":\"c\",\"value\":\"medio\"},"
+        + "{\"skill\":\"mysql\",\"value\":\"alto\"}]");
+    request.addParameter("softSkills", "[{\"softskill\":\"\"},{\"softskill\":\"problem solving\"},"
+        + "{\"softskill\":\"ciaoooooooooooooo\"}]");
+    request.addParameter("lingue", "[{\"lang\":\"inglese\",\"value\":\"A1\"},"
+        + "{\"lang\":\"francese\",\"value\":\"A2\"},"
+        + "{\"lang\":\"tedesco\",\"value\":\"B1\"},{\"lang\":\"spagnolo\",\"value\":\"B2\"}]");
     servlet.doPost(request, response);
     assertEquals("json", response.getContentType());
   }
   
   
   @Test
-  public void test3() throws ServletException, IOException {
-	UserInterface utente = new Student("p.annarumma@studenti.unisa.it", "pasquale", "annarumma", 'M', "pippotto", 0);
-	session.setAttribute("user", utente);
-	request.addParameter("email", "p.annarumma@studenti.unisa.it");
-	request.addParameter("telefono", "3420966986");
+  public void testEmailLenght2() throws ServletException, IOException {
+    UserInterface utente = 
+        new Student("a.napolii@studenti.unisa.it", "antonio", "napoli", 'M', "plutone", 0);
+    session.setAttribute("user", utente);
+    request.addParameter("nome", "pasquale");
+    request.addParameter("cognome", "annarumma");
+    request.addParameter("email", "an@studenti.unisa.it");
+    request.addParameter("telefono", "3420966986");
     request.addParameter("datanascita", "1994-10-10");
-    request.addParameter("skills", "");
-    request.addParameter("softSkills", "");
-    request.addParameter("lingue", "[{\"lang\":\"inglese\",\"value\":\"A1\"},{\"lang\":\"francese\",\"value\":\"A2\"},{\"lang\":\"tedesco\",\"value\":\"B1\"},{\"lang\":\"spagnolo\",\"value\":\"B2\"}]");
-    servlet.doPost(request, response);
-    assertEquals("json", response.getContentType());
-  }
-  
-  
-  @Test
-  public void test4() throws ServletException, IOException {
-	UserInterface utente = new Student("p.annarumma@studenti.unisa.it", "pasquale", "annarumma", 'M', "pippotto", 0);
-	session.setAttribute("user", utente);
-	request.addParameter("email", "p.annarumma@studenti.unisa.it");
-	request.addParameter("telefono", "3420966986");
-    request.addParameter("datanascita", "1994-10-10");
-    request.addParameter("skills", "");
-    request.addParameter("softSkills", "");
-    request.addParameter("lingue", "");
+    request.addParameter("skills", "[{\"skill\":\"java\",\"value\":\"basso\"},"
+        + "{\"skill\":\"c\",\"value\":\"medio\"},"
+        + "{\"skill\":\"mysql\",\"value\":\"alto\"}]");
+    request.addParameter("softSkills", "[{\"softskill\":\"\"},{\"softskill\":\"problem solving\"},"
+        + "{\"softskill\":\"ciaoooooooooooooo\"}]");
+    request.addParameter("lingue", "[{\"lang\":\"inglese\",\"value\":\"A1\"},"
+        + "{\"lang\":\"francese\",\"value\":\"A2\"},"
+        + "{\"lang\":\"tedesco\",\"value\":\"B1\"},{\"lang\":\"spagnolo\",\"value\":\"B2\"}]");
     servlet.doPost(request, response);
     assertEquals("json", response.getContentType());
   }
   
   @Test
-  public void test5() throws ServletException, IOException {
-	UserInterface utente = new Student("p.annarumma@studenti.unisa.it", "pasquale", "annarumma", 'M', "pippotto", 0);
-	session.setAttribute("user", utente);
-	request.addParameter("email", "p.annarumma@studenti.unisa.it");
-	//request.addParameter("telefono", "3420966986");
-    request.addParameter("datanascita", "");
-    request.addParameter("skills", "");
-    request.addParameter("softSkills", "");
-    request.addParameter("lingue", "[{\"lang\":\"inglese\",\"value\":\"A1\"},{\"lang\":\"francese\",\"value\":\"A2\"},{\"lang\":\"tedesco\",\"value\":\"B1\"},{\"lang\":\"spagnolo\",\"value\":\"B2\"}]");
+  public void testTelefonoLenght0() throws ServletException, IOException {
+    UserInterface utente = 
+        new Student("a.napolii@studenti.unisa.it", "antonio", "napoli", 'M', "plutone", 0);
+    session.setAttribute("user", utente);
+    request.addParameter("nome", "pasquale");
+    request.addParameter("cognome", "annarumma");
+    request.addParameter("email", "a.napolii@studenti.unisa.it");
+    request.addParameter("telefono", "");
+    request.addParameter("datanascita", "1994-10-10");
+    request.addParameter("skills", "[{\"skill\":\"java\",\"value\":\"basso\"},"
+        + "{\"skill\":\"c\",\"value\":\"medio\"},"
+        + "{\"skill\":\"mysql\",\"value\":\"alto\"}]");
+    request.addParameter("softSkills", "[{\"softskill\":\"\"},{\"softskill\":\"problem solving\"},"
+        + "{\"softskill\":\"ciaoooooooooooooo\"}]");
+    request.addParameter("lingue", "[{\"lang\":\"inglese\",\"value\":\"A1\"},"
+        + "{\"lang\":\"francese\",\"value\":\"A2\"},"
+        + "{\"lang\":\"tedesco\",\"value\":\"B1\"},{\"lang\":\"spagnolo\",\"value\":\"B2\"}]");
     servlet.doPost(request, response);
     assertEquals("json", response.getContentType());
   }
+  
+  @Test
+  public void testTelefonoLenghtOverflow() throws ServletException, IOException {
+    UserInterface utente = 
+        new Student("a.napolii@studenti.unisa.it", "antonio", "napoli", 'M', "plutone", 0);
+    session.setAttribute("user", utente);
+    request.addParameter("nome", "pasquale");
+    request.addParameter("cognome", "annarumma");
+    request.addParameter("email", "a.napolii@studenti.unisa.it");
+    request.addParameter("telefono", "3420966986123123");
+    request.addParameter("datanascita", "1994-10-10");
+    request.addParameter("skills", "[{\"skill\":\"java\",\"value\":\"basso\"},"
+        + "{\"skill\":\"c\",\"value\":\"medio\"},"
+        + "{\"skill\":\"mysql\",\"value\":\"alto\"}]");
+    request.addParameter("softSkills", "[{\"softskill\":\"\"},{\"softskill\":\"problem solving\"},"
+        + "{\"softskill\":\"ciaoooooooooooooo\"}]");
+    request.addParameter("lingue", "[{\"lang\":\"inglese\",\"value\":\"A1\"},"
+        + "{\"lang\":\"francese\",\"value\":\"A2\"},"
+        + "{\"lang\":\"tedesco\",\"value\":\"B1\"},{\"lang\":\"spagnolo\",\"value\":\"B2\"}]");
+    servlet.doPost(request, response);
+    assertEquals("json", response.getContentType());
+  }
+  
 }
